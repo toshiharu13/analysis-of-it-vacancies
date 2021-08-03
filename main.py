092af868b758481f7502e016f975bacb2883626a
Boyko Alexey <elorian@yandex.ru>
2021-08-03 16:42:01 +0300

    starting write main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from dotenv import load_dotenv
 
@@ -8,3 +9,8 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
     )
+
+HH_TOKEN = os.getenv("HH_TOKEN")
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+url = 'https://api.hh.ru/'
