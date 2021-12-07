a2aba987c685a643782a2ec69ead2f096abdd770
Boyko Alexey <elorian@yandex.ru>
2021-12-07 12:11:58 +0300

    create <prepare_salary_to_table> func refactor code for this func

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ import requests
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
-POPULAR_LANGUAGES = [
-    ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано',
-     'Средняя зарплата'],
-    ['Python', 0, 0, 0],
-    ['Java', 0, 0, 0],
-    ['Javascript', 0, 0, 0],
-    ['PHP', 0, 0, 0],
-    ['C++', 0, 0, 0],
-    ['C#', 0, 0, 0],
-    ['C', 0, 0, 0],
-    ['Go', 0, 0, 0]
-    ]
+POPULAR_LANGUAGES = {
+    'Python',
+    'Java',
+    'Javascript',
+    'PHP',
+    'C++',
+    'C#',
+    'C',
+    'Go'
+}
 
 
 logging.basicConfig(
@@ -73,32 +71,43 @@ def predict_rub_salary_for_hh(vacancy):
     return predict_salary(vacancy_salary, 'from', 'to')
 
 
+def prepare_salary_to_table(
+        programm_lang, vacancies_found, vacancies_processed, average_salary,
+        table_data):
+    if not table_data:
+        table_data.append(
+        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано',
+         'Средняя зарплата'],
+        )
+    table_data.append([programm_lang, vacancies_found, vacancies_processed, average_salary])
+
+
+
 def show_all_vacancy(language):
     response_hh_api = get_amount_of_hh_vacancies(language)
     python_vacancy = []
     python_vacancy += response_hh_api['items']
     pages = response_hh_api['pages']
+    time.sleep(1)
     if pages > 100:  # из расчета 20 вакасий на страницу(глубина <= 2000)
         pages = 100
-    for page in range(1, pages):
+    for page in range(1, 3):
         response_hh_api = get_amount_of_hh_vacancies(language, page)
         python_vacancy += response_hh_api['items']
-        time.sleep(1)
-    logging.info(python_vacancy)
+        time.sleep(2)
+    #logging.info(python_vacancy)
     return (python_vacancy, response_hh_api['found'])
 
 
 if __name__ == "__main__":
     load_dotenv()
     sj_key = os.getenv('SUPERJOB_KEY')
-    hh_table_data = copy.deepcopy(POPULAR_LANGUAGES)
-    sj_table_data = copy.deepcopy(POPULAR_LANGUAGES)
+    hh_table_data = []
+    sj_table_data = []
 
     # Обработка данных HH.ru
-    for programm_lang in hh_table_data:
-        if type(programm_lang[1]) == str:
-            continue
-        all_vacancy_and_found = show_all_vacancy(programm_lang[0])
+    for programm_lang in POPULAR_LANGUAGES:
+        all_vacancy_and_found = show_all_vacancy(programm_lang)
         all_hh_vacancies = all_vacancy_and_found[0]
         vacancies_processed_hh = 0
         average_salary_hh = 0
@@ -108,34 +117,32 @@ if __name__ == "__main__":
             if salary:
                 vacancies_processed_hh += 1
                 average_salary_hh += salary
-        average_salary_hh = average_salary_hh/vacancies_processed_hh
+        average_salary_hh = int(average_salary_hh/vacancies_processed_hh)
 
-        programm_lang[1] = int(vacancies_found_hh)
-        programm_lang[2] = vacancies_processed_hh
-        programm_lang[3] = int(average_salary_hh)
+        prepare_salary_to_table(
+            programm_lang, vacancies_found_hh, vacancies_processed_hh,
+            average_salary_hh,
+            hh_table_data)
         logging.info(f'hh {hh_table_data}')
 
     # Обработка данных API SuperJob
-    for programming_lang in sj_table_data:
-        if type(programming_lang[1]) == str:
-            continue
-        sj_response = get_amount_sj_vacancies(programming_lang[0], sj_key)
+    for programming_lang in POPULAR_LANGUAGES:
+        sj_response = get_amount_sj_vacancies(programming_lang, sj_key)
         all_sj_vacancies = sj_response['objects']
         vacancies_processed = 0
         average_salary = 0
         vacancies_found = sj_response['total']
         for sj_vacancy in all_sj_vacancies:
-            #salary = predict_rub_salary_for_sj(sj_vacancy)
             salary = predict_salary(sj_vacancy, 'payment_from', 'payment_to')
             if salary:
                 vacancies_processed += 1
                 average_salary += salary
             time.sleep(1)
-        average_salary = average_salary / vacancies_processed
+        average_salary = int(average_salary / vacancies_processed)
 
-        programming_lang[1] = int(vacancies_found)
-        programming_lang[2] = vacancies_processed
-        programming_lang[3] = int(average_salary)
+        prepare_salary_to_table(
+            programming_lang, vacancies_found, vacancies_processed,
+            average_salary, sj_table_data)
         logging.info(f'sj {sj_table_data}')
 
     # Вывод данных в виде таблиц
